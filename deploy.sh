89c6430f2a288f6bd2cd6be4b54524924060b4d3
lucagrammer <luca2.minotti@mail.polimi.it>
2022-09-23 19:33:23 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ cd dist
 git init 
 git add -A
 git commit -m 'New Deployment'
-git push -f git@github.com:lucagrammer/lucagrammer.github.io.git master:gh-pages
+git push -f git@github.com:lucagrammer/lucagrammer.github.io.git main:gh-pages
 
 cd -
